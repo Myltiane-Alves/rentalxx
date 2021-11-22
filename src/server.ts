7f1a935a5572dfb2cc35855aab039a76c4549173
Myltiane Alves <myltiane.aux@gmail.com>
2021-11-22 14:00:26 -0300

    Adicionando nova rota

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,10 @@
 import express from 'express';
+import { categoriesRoutes } from './routes/categories.routes';
 
 const app = express();
 
 app.use(express.json());
-app.get("/", (request, response) => {
-    return response.json({ message: "Seja Bem Vindo Myltiane ao Mundo Node Js"})
-});
 
-app.post("/courses", (request, response) => {
-    const { name } = request.body;
-    return response.json({ name });
-});
+app.use(categoriesRoutes);
 
 app.listen(3333, () => console.log("Server is running!")); 
