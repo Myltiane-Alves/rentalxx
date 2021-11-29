204440f12da695de5ab94db453804bb4288251a1
Myltiane Alves <myltiane.aux@gmail.com>
2021-11-29 09:56:45 -0300

    Criando instancia

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -5,10 +5,19 @@ import { ICategoriesRepository, ICreateCategoryDTO } from "./ICategoriesReposito
 class CategoriesRepository implements ICategoriesRepository {
     private categories: Category[];
 
-    constructor() {
+    private static INSTANCE: CategoriesRepository;
+
+    private constructor() {
         this.categories = [];
     }
 
+    public static getInstance(): CategoriesRepository {
+
+        if(!CategoriesRepository.INSTANCE) {
+            CategoriesRepository.INSTANCE = new CategoriesRepository();
+        }
+        return CategoriesRepository.INSTANCE;
+    }
     create({ description, name } : ICreateCategoryDTO): void {
         const category = new Category(); 
     
