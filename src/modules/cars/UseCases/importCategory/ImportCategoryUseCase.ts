f27e39f43240305cc26ce9ec5053e2b1edfa0e0d
Myltiane Alves <myltiane.aux@gmail.com>
2021-12-17 14:01:42 -0300

    Atualizando

--- src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/UseCases/importCategory/ImportCategoryUseCase.ts
@@ -1,16 +1,12 @@
-import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
-import csvParse from 'csv-parse';
+import  csvParse  from 'csv-parse/sync';
 import fs from 'fs';
 
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
 
 interface IImportCategory {
     name: string;
     description: string;
 }
-
-
-
-
 
 class ImportCategoryUseCase {
     constructor(private categoriesRepository: ICategoriesRepository){}
@@ -19,6 +15,7 @@ class ImportCategoryUseCase {
       return new Promise((resolve, reject) => {
         const stream = fs.createReadStream(file.path);
         const categories: IImportCategory[] = [];
+        
         const parseFile = csvParse();
       
         stream.pipe(parseFile);
