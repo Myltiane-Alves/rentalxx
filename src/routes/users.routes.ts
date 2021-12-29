4ecbaf28c1af549b9163d047754021091b0566ff
Myltiane Alves <myltiane.aux@gmail.com>
2021-12-29 11:09:27 -0300

    upload de avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,13 +1,25 @@
+import { Router } from "express";
+import multer from "multer";
+
+
 import { CreateUserController } from "@modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "@modules/cars/useCases/updateUserAvatar/UpdateUserAvatarController";
-import { Router } from "express";
+import uploadConfig from "@config/upload";
+import { ensureAutenticated } from "middlewares/ensureAuthentitcated";
 
 const usersRoutes = Router();
+const uploadAvatar = multer(uploadConfig.upload("./tmp/avatar"));
 
 const createUserController = new CreateUserController();
 const updateUserAvatarController = new UpdateUserAvatarController();
 
 usersRoutes.post("/", createUserController.handle);
 
-usersRoutes.patch("/avatar", updateUserAvatarController.handle)
+usersRoutes.patch(
+    "/avatar",
+    ensureAutenticated,
+    uploadAvatar.single("avatar"),
+    updateUserAvatarController.handle
+);
+
 export { usersRoutes };
