05f332f4881e748827ecaeb76eb8e3b5dffec935
Myltiane Alves <myltiane.aux@gmail.com>
2022-01-04 13:24:12 -0300

    Adicionando test

--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,3 +1,4 @@
+import { AppError } from '../../../../errors/AppError';
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory"
 import { CreateUserUserCase } from "../createUser/CreateUserUseCase";
@@ -29,6 +30,34 @@ describe("Authenticate User", () => {
             email: user.email,
             password: user.password
         });
-        console.log(result);
+        
+        expect(result).toHaveProperty("token")
     });
+
+    it("should not be able to authenticate an nonexistent user", () => {
+        expect(async () => {
+           await authenticateUserUseCase.execute({
+                email: "false@email.com",
+                password: "1234",
+           })
+        }).rejects.toBeInstanceOf(AppError)
+    });
+
+    it("should not be able to authenticate with incorrect password", () => {
+        expect(async() => {
+            const user: ICreateUserDTO = {
+                driver_license: "9999",
+                email: "user@user.com",
+                password: "1234",
+                name: "User Tesy Error"
+            }
+
+            await createUserUserCase.execute(user);
+
+            await authenticateUserUseCase.execute({
+                email: user.email,
+                password: "incorrectPassword"
+            })
+        }).rejects.toBeInstanceOf(AppError)
+    })
 });
