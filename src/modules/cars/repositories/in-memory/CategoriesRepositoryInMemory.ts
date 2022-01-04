50c3298994813c671a509cd376efd308bed926f8
Myltiane Alves <myltiane.aux@gmail.com>
2022-01-04 09:56:05 -0300

    Adicionando novos teste

--- src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CategoriesRepositoryInMemory.ts
@@ -1,4 +1,5 @@
-import { Category } from "@modules/cars/entities/Category";
+
+import { Category } from "../../entities/Category";
 import { ICategoriesRepository, ICreateCategoryDTO } from "../ICategoriesRepository";
 
 
