1f8e307911d7f28b6fd5b51bd02acd7cf789e5c8
Myltiane Alves <myltiane.aux@gmail.com>
2022-01-05 14:55:16 -0300

    Adicionando tdd

--- src/modules/cars/UseCases/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/UseCases/createCar/CreateCarUseCase.spec.ts
@@ -1,15 +1,25 @@
 import "reflect-metadata"
+import { CarsRepositoryInMemory} from "@modules/cars/repositories/in-memory/CarsRepositoryInMemory"
 import { CreateCarUseCase } from "./CreateCarUseCase";
 
 let createCarUseCase: CreateCarUseCase;
-
+let carsRepositoryInMemory: CarsRepositoryInMemory;
 describe("Create Car", () => {
 
     beforeEach(() => {
-        createCarUseCase = new CreateCarUseCase();
+        carsRepositoryInMemory = new CarsRepositoryInMemory();
+        createCarUseCase = new CreateCarUseCase(carsRepositoryInMemory);
     })
 
     it("should be able to create a new car", async () => {
-        await createCarUseCase.execute();
+        await createCarUseCase.execute({
+            name: "Name Car",
+            description: "Description Car",
+            daily_rate: 100,
+            license_plate: "ABC-1134",
+            fine_amount: 60,
+            brand: "Brand",
+            category_id: "category",
+        });
     })
 })
