8ba4579fccd582eb735f6e550e6ce86bfdce313d
Myltiane Alves <myltiane.alves@cleandev.com.br>
2022-03-26 13:43:17 -0300

    feat: continuação do cadastro de aluguel

--- src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
+++ src/modules/rentals/useCases/createRental/CreateRentalUseCase.spec.ts
@@ -1,15 +1,21 @@
+import { RentalsRepositoryInMemory } from "@modules/rentals/repositores/in-memory/RentalsRepositoryInMemory";
 import { CreateRentalUseCase } from "./CreateRentalUseCase";
 
 let createRentalUseCase: CreateRentalUseCase;
+let rentalsRepositoryInMemory: RentalsRepositoryInMemory;
 
 describe("Create Rental", () => {
   beforeEach(() => {
-    createRentalUseCase = new CreateRentalUseCase();
+    rentalsRepositoryInMemory = new RentalsRepositoryInMemory();
+    createRentalUseCase = new CreateRentalUseCase(rentalsRepositoryInMemory);
   });
 
   it("should create a new rental", async () => {
-
-    await createRentalUseCase.execute();
+    await createRentalUseCase.execute({
+      user_id: "12345",
+      car_id: "121212",
+      expected_return_date: new Date(),
+    });
   });
 
 })
